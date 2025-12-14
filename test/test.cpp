30b6addc08d52b32e2fea673890d9b6ed4b5ff0c
riley0122 <77288737+riley0122@users.noreply.github.com>
2025-12-14 15:47:58 +0100

    Add CMRC

--- test/test.cpp
+++ test/test.cpp
@@ -1,13 +1,47 @@
 #include "test_lib.hpp"
+#include <cmrc/cmrc.hpp>
+#include <string>
+#include <iostream>
 
-bool test() {
-    return 1 + 1 == 2;
+#ifndef TAG_FAMILY
+#define TAG_FAMILY ""
+#endif
+
+CMRC_DECLARE(tags);
+
+bool test_access_fs() {
+    auto fs = cmrc::tags::get_filesystem();
+    if (!(&fs)) {
+        return false;
+    }
+    return true;
+}
+
+bool test_tag_exists() {
+    auto fs = cmrc::tags::get_filesystem();
+    std::string path = "tags/";
+    path.append(TAG_FAMILY);
+    path.append("-15.png");
+
+    return fs.exists(path);
+}
+
+bool test_tag_doesnt_exists()
+{
+    auto fs = cmrc::tags::get_filesystem();
+    std::string path = "tags/not-a-real-tag.png";
+    
+    return !fs.exists(path);
 }
 
 int main() {
     return run_tests({
-        test()
+        test_access_fs(),
+        test_tag_exists(),
+        test_tag_doesnt_exists()
     }, {
-        "Test if maths are being done correctly."
+        "Obtaining a handle to the embedded fs.",
+        "Checks if a proper tag exists.",
+        "Checks if an inproper tag doesnt exist."
     });
 }
